376371de48f6b56a330cb7f4db34c79c8827043b
Gulshan Varma <varmagulshan11@gmail.com>
2023-06-11 16:03:02 +0530

    Add project files.

---
+++ ClassLibrary1/CreateTask.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.Xrm.Sdk;
+using System;
+using System.ServiceModel;
+
+namespace ClassLibrary1
+{
+    public class CreateTask:IPlugin
+    {
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            // Obtain the execution context from the service provider.
+            IPluginExecutionContext context = (IPluginExecutionContext)
+                serviceProvider.GetService(typeof(IPluginExecutionContext));
+
+            // The InputParameters collection contains all the data passed in the message request.
+            if (context.InputParameters.Contains("Target") &&  context.InputParameters["Target"] is Entity)
+            {
+                // Obtain the target entity from the input parameters.
+                Entity entity = (Entity)context.InputParameters["Target"];
+
+
+                // Verify that the target entity represents an lead.
+                if (entity.LogicalName != "lead")
+                    return;
+
+                try
+                {
+                    // Create a task activity to follow up with the lead customer in 7 days. 
+                    Entity followup = new Entity("task");
+
+                    followup["subject"] = "Send e-mail to the new customer.";
+                    followup["description"] =
+                        "Follow up with the customer. Check if there are any new issues that need resolution.";
+                    followup["scheduledstart"] = DateTime.Now.AddDays(7);
+                    followup["scheduledend"] = DateTime.Now.AddDays(7);
+                    followup["category"] = context.PrimaryEntityName;
+
+                    // Refer/Associate to the lead in the task activity.
+                    if (context.OutputParameters.Contains("id"))
+                    {
+                        Guid regardingobjectid = new Guid(context.OutputParameters["id"].ToString());
+                        string regardingobjectidType = "lead";
+
+                        followup["regardingobjectid"] = new EntityReference(regardingobjectidType, regardingobjectid);
+                    }
+
+
+                    // Obtain the organization service reference.
+                    IOrganizationServiceFactory serviceFactory = (IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
+                    IOrganizationService service = serviceFactory.CreateOrganizationService(context.UserId);
+
+
+                    // Create the task in Microsoft Dynamics CRM.
+                    service.Create(followup);
+                }
+                catch (FaultException<OrganizationServiceFault> ex)
+                {
+                    throw new InvalidPluginExecutionException("An error occurred in the FollowupPlugin plug-in.", ex);
+                }
+
+                catch (Exception)
+                {
+                    throw;
+                }
+            }
+        }
+    }
+}
